28ddc975ac41919ba990b63debfe552158790bd8
La Min Aung <lamin.aung@infolog.com.sg>
2026-01-24 03:15:59 +0700

    Create standardize response

---
+++ StudentManagement/Wrappers/ApiResponse.cs
@@ -0,0 +1,30 @@
+﻿namespace StudentManagement.Wrappers
+{
+    public class ApiResponse<T>
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string[]? Errors { get; set; }
+        public T Data { get; set; }
+
+        public ApiResponse() { }
+
+        // Constructor for Success
+        public ApiResponse(T data, string message = null)
+        {
+            Succeeded = true;
+            Message = message ?? "Request successful.";
+            Data = data;
+            Errors = null;
+        }
+
+        // Constructor for Failure
+        public ApiResponse(string message, string[]? errors = null)
+        {
+            Succeeded = false;
+            Message = message;
+            Errors = errors;
+            Data = default;
+        }
+    }
+}
