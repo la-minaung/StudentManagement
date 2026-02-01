13f451954c800d8bb27c92c31f05d7ae6b170234
La Min Aung <lamin.aung@infolog.com.sg>
2026-02-01 21:03:23 +0700

    feat: implement ASP.NET Identity and JWT authentication logic

---
+++ StudentManagement/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagement.DTOs;
+using StudentManagement.Services;
+
+namespace StudentManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            var result = await _authService.RegisterUser(dto);
+            if (result)
+            {
+                return Ok("User registered successfully");
+            }
+            return BadRequest("Registration failed. User might already exist.");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            var token = await _authService.Login(dto);
+
+            if (token == null)
+            {
+                return Unauthorized("Invalid username or password.");
+            }
+
+            return Ok(new { Token = token });
+        }
+    }
+}
---
+++ StudentManagement/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using StudentManagement.DTOs;
+
+namespace StudentManagement.Services
+{
+    public interface IAuthService
+    {
+        Task<bool> RegisterUser(RegisterDto registerDto);
+        Task<string?> Login(LoginDto loginDto);
+    }
+}
